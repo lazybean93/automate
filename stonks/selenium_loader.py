2fa2f703a2f5d69268d2b04e938317a1e220d082
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-09-14 14:49:19 +0200

    Stocks

--- stonks/selenium_loader.py
+++ stonks/selenium_loader.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox()
 
 browser.get(sys.argv[1])
-assert sys.argv[3] in browser.title
+#assert sys.argv[3] in browser.title
 
 time.sleep(2)
 
--- stonks/stocks_getValue.sh
+++ stonks/stocks_getValue.sh
@@ -7,13 +7,19 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 sh -c "$LOG \"Start\""
 DATE="$(sh "$HOME""/automation/utils_getDate_ymd_hms.sh")"
-#firefox-esr &
-for i in $(cat "$SCRIPTPATH""/stocks.txt"); do
-	sh -c "$LOG \"$i\""
-	#python3 "$HOME""/stonks/selenium_loader.py" "https://wertpapiere.ing.de/Investieren/Aktie/""$i" "$HOME""/stonks/page.html" "Aktien-Suche"
-	#FUNDAMENTALS1="$(cat "$HOME""/stonks/page.html" | grep '<tbody><tr name="Bid"' | sed 's/EUR/EUR\r\n/g' | sed 's/>/\r\n/g' | grep 'Geldkurs\|Briefkurs\|Tages\|52-Wochen\|EUR' | sed 's/<\/td//g')"
-	#FUNDAMENTALS2="$(cat "$HOME""/stonks/page.html" | grep '<div name="DividendYield">' | sed 's/%/%\r\n/g' | sort | uniq | sed 's/<span class="label">/\r\n/g' | sed 's/</\r\n/g;s/>/\r\n/g' | grep 'Dividendenrendite\|Kurs\|[0-9]')"
-	#echo "$(echo "$i"";""$(echo "$FUNDAMENTALS1"'\r\n'"$FUNDAMENTALS2" | grep -v [a-z] | sed 's/ EUR//g')" | tr -s '\r\n' ';')" "" >> "$DATE"".csv"
+FILE="$SCRIPTPATH""/stocks.txt"
+SUFFIX=".htmlstonks"
+
+for i in $(cat "$FILE" | head -n 100); do
+        INDEX="$(echo $i | tr -d '\r')"
+        sh -c "$LOG \"$i\""
+        tsp python3 "$SCRIPTPATH""/selenium_loader.py" "https://wertpapiere.ing.de/Investieren/Aktie/""$INDEX" "$SCRIPTPATH""/""$INDEX""$SUFFIX" "Aktien-Suche"
+done
+tsp -w
+for i in $(ls *"$SUFFIX"); do
+        FUNDAMENTALS1="$(cat "$i" | grep '<tbody><tr name="Bid"' | sed 's/EUR/EUR\r\n/g' | sed 's/>/\r\n/g' | grep 'Geldkurs\|Briefkurs\|Tages\|52-Wochen\|EUR' | sed 's/<\/td//g')"
+        FUNDAMENTALS2="$(cat "$i" | grep '<div name="DividendYield">' | sed 's/%/%\r\n/g' | sort | uniq | sed 's/<span class="label">/\r\n/g' | sed 's/</\r\n/g;s/>/\r\n/g' | grep 'Dividendenrendite\|Kurs\|[0-9]>
+        echo "$(echo "$i"";""$(echo "$FUNDAMENTALS1"'\r\n'"$FUNDAMENTALS2" | grep -v [a-z] | sed 's/ EUR//g')" | tr -s '\r\n' ';')" "" >> "$SCRIPTPATH""/""$DATE"".csv"
 done
 sh -c "$CLEAN"
 sh -c "$LOG \"End\""
