2557be740f8e29f70ca622c4d9f4dfcb351cae1e
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-11-07 17:29:40 +0100

    Update

--- automation/env.sh
+++ automation/env.sh
@@ -5,8 +5,8 @@ export KEY_RETURN="sh ""$HOME""/automation/utils_sendkey_Return.sh"
 export HIDEFILE="/dev/zero"
 export HIDEFILE_CHECK_DELETE="sh ""$HOME""/automation/utils_hidefile_check_delete.sh"
 export LOG="sh ""$HOME""/automation/utils_log.sh $0"
+
 SCREENRESOLUTION="$(ps aux | grep -v grep | grep -i Xtightvnc | sed 's/ -geometry /\n/g' | tail -n1 | sed 's/ /\n/g' | head -n1 | sed 's/x/\n/g')"
-export SCREENRESOLUTION
 if [ -n "$SCREENRESOLUTION" ]; then
 	export SCREENHEIGHT=$(echo "$SCREENRESOLUTION" | tail -n 1)
 	export SCREENWIDTH=$(echo "$SCREENRESOLUTION" | head -n 1)
@@ -14,5 +14,6 @@ else
 	export SCREENHEIGHT=$(xrandr | head -n1 | sed 's/current /\n/' | tail -n1 | sed 's/, /\n/g' | head -n1 | sed 's/ x /\n/' | head -n2 | tail -n 1)
 	export SCREENWIDTH=$(xrandr | head -n1 | sed 's/current /\n/' | tail -n1 | sed 's/, /\n/g' | head -n1 | sed 's/ x /\n/' | head -n1)
 fi
+
 export WAITLOADED="nice -n 19 sh ""$HOME""/automation/utils_waitLoaded_loop.sh"
 export WAITLOADED_SILENT="nice -n 19 sh ""$HOME""/automation/utils_waitLoaded_loop_silent.sh"
