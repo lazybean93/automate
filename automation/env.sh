75c9ebac0d7909d8e45301ceecc4e63c89f6f594
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-11-07 17:10:37 +0100

    Update

--- automation/env.sh
+++ automation/env.sh
@@ -5,7 +5,7 @@ export KEY_RETURN="sh ""$HOME""/automation/utils_sendkey_Return.sh"
 export HIDEFILE="/dev/zero"
 export HIDEFILE_CHECK_DELETE="sh ""$HOME""/automation/utils_hidefile_check_delete.sh"
 export LOG="sh ""$HOME""/automation/utils_log.sh $0"
-SCREENRESOLUTION="$(ps aux | grep -v grep | grep -i Xtightvnc)"
+SCREENRESOLUTION="$(ps aux | grep -v grep | grep -i Xtightvnc | sed 's/ -geometry /\n/g' | tail -n1)"
 export SCREENRESOLUTION
 export SCREENHEIGHT=$(xrandr | head -n1 | sed 's/current /\n/' | tail -n1 | sed 's/, /\n/g' | head -n1 | sed 's/ x /\n/' | head -n2 | tail -n 1)
 export SCREENWIDTH=$(xrandr | head -n1 | sed 's/current /\n/' | tail -n1 | sed 's/, /\n/g' | head -n1 | sed 's/ x /\n/' | head -n1)
