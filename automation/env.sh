727993e3711fd2ad0fcab7b475e16fba47dfd23a
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-11-07 17:27:29 +0100

    Update

--- automation/env.sh
+++ automation/env.sh
@@ -8,11 +8,11 @@ export LOG="sh ""$HOME""/automation/utils_log.sh $0"
 SCREENRESOLUTION="$(ps aux | grep -v grep | grep -i Xtightvnc | sed 's/ -geometry /\n/g' | tail -n1 | sed 's/ /\n/g' | head -n1 | sed 's/x/\n/g')"
 export SCREENRESOLUTION
 if [ -n "$SCREENRESOLUTION" ]; then
-	echo true
+	export SCREENHEIGHT=$($SCREENRESOLUTION | tail -n 1)
+	export SCREENWIDTH=$($SCREENRESOLUTION | head -n 1)
 else
-	echo false
+	export SCREENHEIGHT=$(xrandr | head -n1 | sed 's/current /\n/' | tail -n1 | sed 's/, /\n/g' | head -n1 | sed 's/ x /\n/' | head -n2 | tail -n 1)
+	export SCREENWIDTH=$(xrandr | head -n1 | sed 's/current /\n/' | tail -n1 | sed 's/, /\n/g' | head -n1 | sed 's/ x /\n/' | head -n1)
 fi
-export SCREENHEIGHT=$(xrandr | head -n1 | sed 's/current /\n/' | tail -n1 | sed 's/, /\n/g' | head -n1 | sed 's/ x /\n/' | head -n2 | tail -n 1)
-export SCREENWIDTH=$(xrandr | head -n1 | sed 's/current /\n/' | tail -n1 | sed 's/, /\n/g' | head -n1 | sed 's/ x /\n/' | head -n1)
 export WAITLOADED="nice -n 19 sh ""$HOME""/automation/utils_waitLoaded_loop.sh"
 export WAITLOADED_SILENT="nice -n 19 sh ""$HOME""/automation/utils_waitLoaded_loop_silent.sh"
