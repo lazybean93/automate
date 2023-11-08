008c4c05fa8b5d2f270f683791b3bf2f3ad9310f
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-11-08 07:53:22 +0100

    Update

--- pancake/pancake_cron.sh
+++ pancake/pancake_cron.sh
@@ -1,7 +1,10 @@
 crontab -l > "$HOME""/mycron"
+
 echo "0 6 * * 1-5 for i in \$(seq 1 3); do tsp sh "$HOME""/pancake/pancake_start_BatteryCharged.sh"; done" >> "$HOME""/mycron"
 echo "0 7 * * 6-7 for i in \$(seq 1 2); do tsp sh "$HOME""/pancake/pancake_start_BatteryCharged.sh"; done" >> "$HOME""/mycron"
 echo "0 10 * * * tsp sh "$HOME""/pancake/pancake_toTarget_BatteryCharged_loadMapUp.sh"" >> "$HOME""/mycron"
 echo "0 22 * * * tsp sh "$HOME""/pancake/pancake_cancelEverything.sh"" >> "$HOME""/mycron"
+echo "" >> "$HOME""/mycron"
+
 crontab "$HOME""/mycron"
 rm "$HOME""/mycron"
