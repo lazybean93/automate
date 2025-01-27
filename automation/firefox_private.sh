94f0b621a01c873d33d9738ee43849f2b7498247
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-01-27 18:47:40 +0100

    updated firefox_private.sh

--- automation/firefox_private.sh
+++ automation/firefox_private.sh
@@ -51,5 +51,8 @@ if [ -z "$(cat "$WEBPAGE" | sed 's/>/>\n/g' | grep 'false</span>')" ]; then
 	xte "key Tab"
 	"$SHELL" -c "$KEY_RETURN"
 	xte "keydown Control_L" "key W" "keyup Control_L"
+	"$SHELL" -c "$WAITLOADED 1"
+	xte "keydown Control_L" "key W" "keyup Control_L"
+	"$SHELL" -c "$WAITLOADED 1"
 fi
 "$SHELL" -c "$LOG \"End\""
