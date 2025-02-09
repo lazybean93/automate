868f72ef8e2c3f1aa1f54ee1662499d2afedda72
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-02-09 19:56:01 +0100

    updated firefox_private.sh

--- automation/firefox_private.sh
+++ automation/firefox_private.sh
@@ -11,7 +11,7 @@ for i in $(seq 1 10); do
 	if [ -n "$(ps -ely | grep firefox)" ]; then
 		sleep 1
 		"$SHELL" -c "$KEY_RETURN" > /dev/zero 2>&1
-		wmctrl -c "$(wmctrl -l | grep Firefox | sed 's/automate /\n/g' | tail -n1)"
+		wmctrl -c "$(wmctrl -l | grep Firefox | sed 's/ /\n/g' | head -n 1)"
 		"$SHELL" -c "$LOG \"Another Round\""
 	else
 		return
