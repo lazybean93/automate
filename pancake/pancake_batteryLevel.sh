7d595142fa6853b6b823f6ecad7fce844824184b
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-02-09 20:50:00 +0100

    updated firefox_private.sh

--- pancake/pancake_batteryLevel.sh
+++ pancake/pancake_batteryLevel.sh
@@ -1,8 +1,8 @@
 . "$HOME""/automation/env.sh"
 
 #"$SHELL" -c "$LOG \"Start\""
-sh "$HOME""/pancake/pancake_startpage.sh" > $HIDEFILE 2>&1
-WEBPAGE="$(sh "$HOME""/automation/utils_saveWebsite.sh")"
+"$SHELL" "$HOME""/pancake/pancake_startpage.sh"
+WEBPAGE="$("$SHELL" "$HOME""/automation/utils_saveWebsite.sh")"
 BATTERYLEVEL=$(cat "$WEBPAGE" | grep battery-status-text-value\" | sed 's/value">/\n/g' | tail -n1 | sed 's/</\n/' | head -n1)
 STATE=$(cat "$WEBPAGE" | grep "\"robot-state\"" | sed 's/>/\n/' | tail -n1 | sed 's/</\n/' | head -n1)
 rm "$WEBPAGE"
