2ae86bd8c1e113d1c8c36813dfdbd7977d027ef8
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-02-09 14:48:08 +0100

    Updated pancake_start_batteryCharged.sh

--- pancake/pancake_start_BatteryCharged.sh
+++ pancake/pancake_start_BatteryCharged.sh
@@ -1,1 +1,1 @@
-sh "$HOME""/pancake/pancake_runScript_batteryCharged.sh" 80 "sh ""$HOME""/pancake/pancake_start.sh"
+sh "$HOME""/pancake/pancake_runScript_batteryCharged.sh" 80 "$HOME""/pancake/pancake_start.sh"
