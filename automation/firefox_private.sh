b4dabe90b8fe92c0652b6e65b80ba5e33fcc9c48
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-02-09 18:04:07 +0100

    updated firefox_private.sh

--- automation/firefox_private.sh
+++ automation/firefox_private.sh
@@ -3,6 +3,27 @@
 $SHELL -c "$LOG \"Start: loading $1\""
 firefox-esr --private-window $1 2>/dev/zero &
 sleep 2;
+killall firefox-esr > /dev/zero 2>&1
+killall crashreporter > /dev/zero 2>&1
+sleep 1
+rm -r "$HOME""/.ssh"
+for i in $(seq 1 10); do
+	if [ -n "$(ps -ely | grep firefox)" ]; then
+		sleep 1
+		"$SHELL" -c "$KEY_RETURN" > /dev/zero 2>&1
+		wmctrl -c "$(wmctrl -l | grep Firefox | sed 's/automate /\n/g' | tail -n1)"
+		"$SHELL" -c "$LOG \"Another Round\""
+	else
+		return
+	fi
+done
+
+echo 'user_pref("browser.translations.automaticallyPopup", false);' >> .mozilla/firefox/*/prefs.js;
+echo 'user_pref("security.sandbox.warn_unprivileged_namespaces", false);' >> .mozilla/firefox/*/prefs.js;
+
+firefox-esr --private-window $1 2>/dev/zero &
+sleep 2;
+
 ps -ely | grep firefox
 if [ -n "$(ps -ely | grep defunct)" ]; then
 	"$SHELL" -c "$CLEAN"
@@ -26,32 +47,9 @@ if [ -z "$(ps -ely | grep firefox)" ]; then
 	"$SHELL" -c "sh $0 $1"
 	exit
 fi
-xte "keydown Control_L" "key T" "keyup Control_L"
-"$SHELL" -c "$WAITLOADED 1"
-xte "str about:config"
-"$SHELL" -c "$KEY_RETURN"
-xte "keydown Control_L" "key F" "keyup Control_L"
-"$SHELL" -c "$WAITLOADED 1"
-xte "str these preferences"
-"$SHELL" -c "$WAITLOADED 1"
-xte "key Escape"
-"$SHELL" -c "$WAITLOADED 1"
-xte "key Tab"
-"$SHELL" -c "$KEY_RETURN"
-xte "str browser.translations.automaticallyPopup"
-"$SHELL" -c "$WAITLOADED 1"
-WEBPAGE=$(sh "$HOME""/automation/utils_saveWebsite.sh")
-if [ -z "$(cat "$WEBPAGE" | sed 's/>/>\n/g' | grep 'false</span>')" ]; then
-	xte "keydown Control_L" "key F" "keyup Control_L"
-	"$SHELL" -c "$WAITLOADED 1"
-	xte "str true"
-	"$SHELL" -c "$WAITLOADED 1"
-	xte "key Escape"
-	"$SHELL" -c "$WAITLOADED 1"
-	xte "key Tab"
-	"$SHELL" -c "$KEY_RETURN"
-fi
-rm "$WEBPAGE"
-xte "keydown Control_L" "key W" "keyup Control_L"
+
 "$SHELL" -c "$WAITLOADED 1"
 "$SHELL" -c "$LOG \"End\""
+
+#user_pref("browser.translations.automaticallyPopup", false);
+#user_pref("security.sandbox.warn_unprivileged_namespaces", false);
