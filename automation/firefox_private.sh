ca983da642cc46998b23631e34dc8ea149723eef
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-02-09 20:37:09 +0100

    updated firefox_private.sh

--- automation/firefox_private.sh
+++ automation/firefox_private.sh
@@ -19,12 +19,12 @@ for i in $(seq 1 10); do
 done
 
 $SHELL -c "$LOG \"Sleep 5\""
-echo "ls "$HOME"/.mozilla/firefox/"
-echo "$(ls "$HOME"/.mozilla/firefox/)"
-echo 'user_pref("browser.translations.automaticallyPopup", false);' >> "$HOME"/.mozilla/firefox/*/prefs.js;
+echo "ls $HOME/.mozilla/firefox/"
+ls $HOME/.mozilla/firefox/
+#echo 'user_pref("browser.translations.automaticallyPopup", false);' >> "$HOME"/.mozilla/firefox/*/prefs.js;
 
 $SHELL -c "$LOG \"Sleep 5\""
-echo 'user_pref("security.sandbox.warn_unprivileged_namespaces", false);' >> "$HOME"/.mozilla/firefox/*/prefs.js;
+#echo 'user_pref("security.sandbox.warn_unprivileged_namespaces", false);' >> "$HOME"/.mozilla/firefox/*/prefs.js;
 
 
 $SHELL -c "$LOG \"Sleep 5\""
