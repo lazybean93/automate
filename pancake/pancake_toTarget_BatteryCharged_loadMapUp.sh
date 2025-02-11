f35ecf70ee60fdd3a3da92f164369b229b9d1f30
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-02-11 10:42:04 +0100

    updated pancake

--- pancake/pancake_toTarget_BatteryCharged_loadMapUp.sh
+++ pancake/pancake_toTarget_BatteryCharged_loadMapUp.sh
@@ -1,1 +1,1 @@
-sh "$HOME""/pancake/pancake_runScript_batteryCharged.sh" 100 "sh ""$HOME""/pancake/pancake_toTarget_loadMapUp.sh"
+sh "$HOME""/pancake/pancake_runScript_batteryCharged.sh" 100 "$HOME""/pancake/pancake_toTarget_loadMapUp.sh"
