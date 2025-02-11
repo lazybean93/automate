f852cdc8efe6393fbb2762b41d01e32030249e96
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-02-11 18:33:25 +0100

    updated pancake

--- pancake/pancake_batteryLevel.sh
+++ pancake/pancake_batteryLevel.sh
@@ -3,7 +3,6 @@
 #"$SHELL" -c "$LOG \"Start\""
 "$SHELL" "$HOME""/pancake/pancake_startpage.sh"
 WEBPAGE="$("$SHELL" "$HOME""/automation/utils_saveWebsite.sh")"
-sleep infinity
 BATTERYLEVEL=$(cat "$WEBPAGE" | grep battery-status-text-value\" | sed 's/value">/\n/g' | tail -n1 | sed 's/</\n/' | head -n1)
 STATE=$(cat "$WEBPAGE" | grep "\"robot-state\"" | sed 's/>/\n/' | tail -n1 | sed 's/</\n/' | head -n1)
 rm "$WEBPAGE"
