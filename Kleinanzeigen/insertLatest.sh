3619c87068d30833024458af6aaf7b8584ccec07
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-07 22:44:06 +0100

    modified:   Kleinanzeigen/insertLatest.sh

--- Kleinanzeigen/insertLatest.sh
+++ Kleinanzeigen/insertLatest.sh
@@ -144,7 +144,7 @@ password=`echo str "$(cat "$HOME""/Kleinanzeigen/credentials.txt" | tail -n1)"`
   "$SHELL" -c "$LOG \"5. Description\""
         DESCRIPTION="$(cat "$WEBPAGE" | grep -A1 'itemprop="description">' | grep -v 'itemprop="description">' | xargs | sed 's/<\/p>//g;s/<br>/\n/g' | sed 's/\&amp;/\&/g')"
   "$SHELL" -c "$LOG \"6. Category\""
-        CATEGORY="$(cat "$WEBPAGE" | grep breadcrump-link | sed 's/title">/\n/g;s/<\/span><\/a>//g' | grep -v '<a class' | sed 's/\&amp;/\&/g')"
+        CATEGORY="$(cat "$WEBPAGE" | grep breadcrump-link | sed 's/title">/\n/g;s/<\/span><\/a>//g' | grep -v '<a class' | sed 's/\&amp;/\&/g'; cat "$WEBPAGE" | grep -C1 'Art<span' | tail -n1 | sed 's/    //g;s/<\/span>//g;s/\&amp;/\&/g')"
 
 "$SHELL" -c "$LOG \"Insert item\""
  "$SHELL" -c "$LOG \"1. Open \"Anzeige Aufgeben\"\""
