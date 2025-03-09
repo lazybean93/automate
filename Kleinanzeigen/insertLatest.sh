99f5dfd3d2c9be383313f8f816357ae4073bbd34
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-09 15:13:05 +0100

    modified:   Kleinanzeigen/insertLatest.sh

--- Kleinanzeigen/insertLatest.sh
+++ Kleinanzeigen/insertLatest.sh
@@ -326,8 +326,11 @@ password=`echo str "$(cat "$HOME""/Kleinanzeigen/credentials.txt" | tail -n1)"`
         		keyEscape
 				keyTab
 				waitloaded_1
-				keyReturn
+				keyTab
 				waitloaded_1
+				xte "keydown Shift_L" "key Tab" "keyup Shift_L"
+				waitloaded_1
+				keyReturn
 				echo "$CATEGORY"
 				"$SHELL" -c "$LOG \"Küche & Esszimmer\""
 				fail
