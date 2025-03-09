4561a0e97a570f8800b5f11c0f461429e5c11fb5
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-09 14:43:27 +0100

    modified:   Kleinanzeigen/insertLatest.sh

--- Kleinanzeigen/insertLatest.sh
+++ Kleinanzeigen/insertLatest.sh
@@ -317,7 +317,7 @@ password=`echo str "$(cat "$HOME""/Kleinanzeigen/credentials.txt" | tail -n1)"`
 				waitloaded_1
 				xte "str Weiter"
 				waitloaded_1
-				#keyTab
+				keyTab
 				#waitloaded_1
 				#keyReturn
 				#waitloaded_1
