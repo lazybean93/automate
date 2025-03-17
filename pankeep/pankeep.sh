4cf110d60fe7ccc15aa9f97e6661e774560f3d7d
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-17 16:49:41 +0100

    modified:   pankeep/pankeep.sh

--- pankeep/pankeep.sh
+++ pankeep/pankeep.sh
@@ -3,9 +3,11 @@
 #"$SHELL" -c "$LOG \"Start\""
 #"$SHELL" "$HOME""/pancake/pancake_startpage.sh"
 WEBPAGE="$("$SHELL" "$HOME""/automation/utils_saveWebsite.sh")"
-BATTERYLEVEL=$(cat "$WEBPAGE" | grep battery-status-text-value\" | sed 's/value">/\n/g' | tail -n1 | sed 's/</\n/' | head -n1)
-STATE=$(cat "$WEBPAGE" | grep "\"robot-state\"" | sed 's/>/\n/' | tail -n1 | sed 's/</\n/' | head -n1)
-cat "$WEBPAGE"
+if [ "$(cat "$WEBPAGE" | grep Ladefehler)" ]; then
+    echo bla
+else
+    echo blubb
+fi
 
 #Leerlauf
 #Reinigung
