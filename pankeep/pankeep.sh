afd5f3ea29229b15f5e7bedc6b90cf02ecfe3a25
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-17 16:46:58 +0100

    modified:   pankeep/pankeep.sh

--- pankeep/pankeep.sh
+++ pankeep/pankeep.sh
@@ -5,11 +5,12 @@
 WEBPAGE="$("$SHELL" "$HOME""/automation/utils_saveWebsite.sh")"
 BATTERYLEVEL=$(cat "$WEBPAGE" | grep battery-status-text-value\" | sed 's/value">/\n/g' | tail -n1 | sed 's/</\n/' | head -n1)
 STATE=$(cat "$WEBPAGE" | grep "\"robot-state\"" | sed 's/>/\n/' | tail -n1 | sed 's/</\n/' | head -n1)
-rm "$WEBPAGE"
+cat "$WEBPAGE"
+
 #Leerlauf
 #Reinigung
-if [ "$STATE" = "Lädt" ]; then
-	echo $BATTERYLEVEL
-else
-	echo a_$STATE
-fi
+#if [ "$STATE" = "Lädt" ]; then
+#	echo $BATTERYLEVEL
+#else
+#	echo a_$STATE
+#fi
