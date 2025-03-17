fd5897dd8ec0a7d454d76fc4c86cd18356b8f46b
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-17 19:02:44 +0100

    modified:   Kleinanzeigen/insertLatest.sh

--- Kleinanzeigen/insertLatest.sh
+++ Kleinanzeigen/insertLatest.sh
@@ -1,1 +1,6 @@
-bash "$HOME""/kleinanzeigen/insertLatest_run.sh"
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+
+bash "$SCRIPTPATH""/insertLatest_run.sh"
