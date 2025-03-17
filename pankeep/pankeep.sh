ea032d4d82b67144d1c2db45e1a5cf797debf329
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-17 16:45:03 +0100

    modified pankeep.sh

--- pankeep/pankeep.sh
+++ pankeep/pankeep.sh
@@ -1,7 +1,7 @@
 . "$HOME""/automation/env.sh"
 
 #"$SHELL" -c "$LOG \"Start\""
-"$SHELL" "$HOME""/pancake/pancake_startpage.sh"
+#"$SHELL" "$HOME""/pancake/pancake_startpage.sh"
 WEBPAGE="$("$SHELL" "$HOME""/automation/utils_saveWebsite.sh")"
 BATTERYLEVEL=$(cat "$WEBPAGE" | grep battery-status-text-value\" | sed 's/value">/\n/g' | tail -n1 | sed 's/</\n/' | head -n1)
 STATE=$(cat "$WEBPAGE" | grep "\"robot-state\"" | sed 's/>/\n/' | tail -n1 | sed 's/</\n/' | head -n1)
