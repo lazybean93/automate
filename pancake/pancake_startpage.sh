d201221f83d8abf6e7a975b85d38acf74915e557
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-17 17:11:57 +0100

    modified:   pancake/pancake_startpage.sh

--- pancake/pancake_startpage.sh
+++ pancake/pancake_startpage.sh
@@ -1,1 +1,3 @@
-sh "$HOME""/automation/utils_startpage.sh" http://192.168.178.40/
+if [ -n $(ps -ef | grep firefox-esrd | grep -v grep) ]; then 
+    sh "$HOME""/automation/utils_startpage.sh" http://192.168.178.40/
+fi
