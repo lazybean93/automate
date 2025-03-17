c7815d7d8c62e5499981745e6e9450b8f35e661b
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-17 17:04:57 +0100

    modified:   pankeep/pankeep_cron.sh

--- pankeep/pankeep_cron.sh
+++ pankeep/pankeep_cron.sh
@@ -1,6 +1,6 @@
 crontab -l > "$HOME""/mycron"
 
-echo "* * * * * tsp sh "$HOME""/pankeep/pankeep.sh"; done" >> "$HOME""/mycron"
+echo "* * * * * tsp sh "$HOME""/pankeep/pankeep.sh"" >> "$HOME""/mycron"
 #echo "0 7 * * 6-7 for i in \$(seq 1 2); do tsp sh "$HOME""/pancake/pancake_start_BatteryCharged.sh"; done" >> "$HOME""/mycron"
 #echo "0 10 * * * tsp sh "$HOME""/pancake/pancake_toTarget_BatteryCharged_loadMapUp.sh"" >> "$HOME""/mycron"
 #echo "0 22 * * * tsp sh "$HOME""/pancake/pancake_cancelEverything.sh"" >> "$HOME""/mycron"
