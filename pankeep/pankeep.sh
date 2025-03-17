275c4a6d3df4711db85e12eef2090080450bffe4
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-03-17 17:02:11 +0100

    modified:   pankeep/pankeep.sh

--- pankeep/pankeep.sh
+++ pankeep/pankeep.sh
@@ -4,9 +4,13 @@
 #"$SHELL" "$HOME""/pancake/pancake_startpage.sh"
 WEBPAGE="$("$SHELL" "$HOME""/automation/utils_saveWebsite.sh")"
 if [ -n "$(cat "$WEBPAGE" | grep Ladefehler)" ]; then
-    echo bla
-else
-    echo blubb
+    RIGHTBUTTONX=$(($(($SCREENWIDTH/2))+50))
+    UPPERROW=300;
+    ROWDELTA=50;
+    PREFIX="xte 'mousemove "
+    SUFFIX="' 'mouseclick 1'"
+    BUTTONSTATION="$PREFIX$RIGHTBUTTONX $(($UPPERROW+$ROWDELTA*1))$SUFFIX"
+    "$SHELL" -c "$BUTTONSTATION"
 fi
 rm "$WEBPAGE"
 
