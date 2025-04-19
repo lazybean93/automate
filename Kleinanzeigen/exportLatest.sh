f27c14e2659131b0a06a3cc2d440f5b02ebd5f26
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-04-19 14:43:30 +0200

    modified:   Kleinanzeigen/exportLatest.sh

--- Kleinanzeigen/exportLatest.sh
+++ Kleinanzeigen/exportLatest.sh
@@ -60,8 +60,10 @@ password=`echo str "$(cat "$HOME""/Kleinanzeigen/credentials.txt" | tail -n1)"`
 "$SHELL" -c "$LOG \"Start\""
 	"$SHELL" "$HOME""/automation/utils_startpage.sh" "https://kleinanzeigen.de"
  	strg_f
- 	xte "str Ablehnen"
+ 	xte "str Impressum"
  	waitloaded_1
+	exit
+	
  	keyEscape
  	keyReturn
 "$SHELL" -c "$LOG \"Enter Credentials\""
