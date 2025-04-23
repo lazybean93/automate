88b2d10888f3557349cbeac0e2e5ae5748ce14f5
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-04-23 19:29:21 +0200

    modified:   Kleinanzeigen/exportLatest.sh modified:   Kleinanzeigen/login.sh

--- Kleinanzeigen/exportLatest.sh
+++ Kleinanzeigen/exportLatest.sh
@@ -5,38 +5,8 @@
 mail=`echo str "$(cat "$HOME""/credentials/kleinanzeigen.txt" | head -n1)"`
 password=`echo str "$(cat "$HOME""/credentials/kleinanzeigen.txt" | tail -n1)"`
 
-"$SHELL" -c "$LOG \"Start\""
-    "$SHELL" "$HOME""/automation/utils_startpage.sh" "https://kleinanzeigen.de"
-    "$SHELL" -c "$KEY_SEARCH"
-    "$SHELL" -c "$WAITLOADED 1"
-    xte "str Impressum"
-    "$SHELL" -c "$WAITLOADED 1"
-    "$SHELL" -c "$KEY_ESCAPE"
-    "$SHELL" -c "$WAITLOADED 1"
-    for i in $(seq 1 3); do
-        xte "keydown Shift_L" "key Tab" "keyup Shift_L"
-    done
-    "$SHELL" -c "$WAITLOADED 1"
-    "$SHELL" -c "$KEY_RETURN"; "$SHELL" "$HOME""/automation/firefox_status.sh"; if [ $? -ne 0 ]; then sh $0; exit 0; fi
-    for i in $(seq 1 3); do
-        xte "key Tab"
-    done
-    "$SHELL" -c "$WAITLOADED 1"
-    "$SHELL" -c "$KEY_RETURN"; "$SHELL" "$HOME""/automation/firefox_status.sh"; if [ $? -ne 0 ]; then sh $0; exit 0; fi
-
-"$SHELL" -c "$LOG \"Enter Credentials\""
-    "$SHELL" -c "$WAITLOADED 1"
-    xte "$mail"
-    "$SHELL" -c "$WAITLOADED 1"
-    "$SHELL" -c "$KEY_TAB"
-    "$SHELL" -c "$WAITLOADED 1"
-    xte "$password"
-    "$SHELL" -c "$WAITLOADED 1"
-    for i in $(seq 1 2); do
-        "$SHELL" -c "$KEY_TAB"
-    done;
-    "$SHELL" -c "$WAITLOADED 1"
-    "$SHELL" -c "$KEY_RETURN"; "$SHELL" "$HOME""/automation/firefox_status.sh"; if [ $? -ne 0 ]; then sh $0; exit 0; fi
+"$SHELL" -c "$LOG \"Login\""
+    "$SHELL" -c "$HOME""/Kleinanzeigen/login.sh"
     exit
 
 "$SHELL" -c "$LOG \"Get to last Item\""
--- Kleinanzeigen/login.sh
+++ Kleinanzeigen/login.sh
@@ -1,48 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
 . "$HOME""/automation/env.sh"
 
-mail=`echo str "$(cat "$HOME""/Kleinanzeigen/credentials.txt" | head -n1)"`
-password=`echo str "$(cat "$HOME""/Kleinanzeigen/credentials.txt" | tail -n1)"`
+mail=`echo str "$(cat "$HOME""/credentials/kleinanzeigen.txt" | head -n1)"`
+password=`echo str "$(cat "$HOME""/credentials/kleinanzeigen.txt" | tail -n1)"`
 
 "$SHELL" -c "$LOG \"Start\""
-	"$SHELL" "$HOME""/automation/utils_startpage.sh" "https://kleinanzeigen.de"
- 	xte "keydown Control_L" "key F" "keyup Control_L"
+    "$SHELL" "$HOME""/automation/utils_startpage.sh" "https://kleinanzeigen.de"
+    "$SHELL" -c "$KEY_SEARCH"
     "$SHELL" -c "$WAITLOADED 1"
- 	xte "str tippen sie zum Ablehnen bitte hier"
- 	"$SHELL" -c "$WAITLOADED 1"
- 	xte "key Escape"
-	"$SHELL" -c "$WAITLOADED 1"
- 	"$SHELL" -c "$KEY_RETURN";
-	"$SHELL" "$HOME""/automation/firefox_status.sh"
-	if [ $? -ne 0 ]; then
-		"$SHELL" $0;
-		exit 0;
-	fi
- "$SHELL" -c "$LOG \"Enter Credentials\""
- 	for i in $(seq 1 5); do
- 		"$SHELL" -c "$KEY_TAB"
- 	done;
- 	"$SHELL" -c "$WAITLOADED 1"
- 	"$SHELL" -c "$KEY_RETURN"
-	"$SHELL" "$HOME""/automation/firefox_status.sh"
-	if [ $? -ne 0 ]; then
-		"$SHELL" $0;
-		exit 0;
-	fi
- 	xte "$mail"
- 	"$SHELL" -c "$WAITLOADED 1"
- 	"$SHELL" -c "$KEY_TAB"
- 	"$SHELL" -c "$WAITLOADED 1"
- 	xte "$password"
- 	"$SHELL" -c "$WAITLOADED 1"
- 	for i in $(seq 1 2); do
- 		"$SHELL" -c "$KEY_TAB"
- 	done;
- 	"$SHELL" -c "$WAITLOADED 1"
- 	"$SHELL" -c "$KEY_RETURN";
-	"$SHELL" "$HOME""/automation/firefox_status.sh";
-	if [ $? -ne 0 ]; then
-		"$SHELL" $0;
-		exit 0;
-	fi
+    xte "str Impressum"
+    "$SHELL" -c "$WAITLOADED 1"
+    "$SHELL" -c "$KEY_ESCAPE"
+    "$SHELL" -c "$WAITLOADED 1"
+    for i in $(seq 1 3); do
+        xte "keydown Shift_L" "key Tab" "keyup Shift_L"
+    done
+    "$SHELL" -c "$WAITLOADED 1"
+    "$SHELL" -c "$KEY_RETURN"
+    for i in $(seq 1 3); do
+        xte "key Tab"
+    done
+    "$SHELL" -c "$WAITLOADED 1"
+    "$SHELL" -c "$KEY_RETURN"
+
+"$SHELL" -c "$LOG \"Enter Credentials\""
+    "$SHELL" -c "$WAITLOADED 1"
+    xte "$mail"
+    "$SHELL" -c "$WAITLOADED 1"
+    "$SHELL" -c "$KEY_TAB"
+    "$SHELL" -c "$WAITLOADED 1"
+    xte "$password"
+    "$SHELL" -c "$WAITLOADED 1"
+    for i in $(seq 1 2); do
+        "$SHELL" -c "$KEY_TAB"
+    done;
+    "$SHELL" -c "$WAITLOADED 1"
+    "$SHELL" -c "$KEY_RETURN"
