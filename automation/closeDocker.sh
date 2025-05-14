8734846c77937844fe1e9c07ee4bb074ede91b52
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-05-14 15:46:49 +0200

    modified:   automation/closeDocker.sh

--- automation/closeDocker.sh
+++ automation/closeDocker.sh
@@ -3,5 +3,6 @@
 #expect "Are you sure you want to continue connecting (yes/no/[fingerprint])?"
 
 spawn ssh oliver@192.168.178.37
+expect "?"
 interact
 #expect "yes/no" { send "yes\r" expect "*?assword" { send "[lindex $argv 2]\r" } } "*?assword" { send "[lindex $argv 2]\r" } expect "# " { send "su - [lindex $argv 3]\r" } expect ": " { send "[lindex $argv 4]\r" } expect "# " { send "ls -ltr\r" } interact
