6a1b404e429095fe714c35ebbdd80a7e01213a2c
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-05-14 13:42:54 +0200

    modified:   automation/env.sh

--- automation/env.sh
+++ automation/env.sh
@@ -1,12 +1,14 @@
+$AUTOMATION="$HOME""/automation"
+
 export DISPLAY="$(ps aux | grep -v grep | grep -i 'Xorg\|Xtightvnc' | sed 's/ :/\n:/g' | tail -n1 | sed 's/ /\n/g' | head -n1)"
 
-export CLEAN="sh ""$HOME""/automation/utils_clean.sh"
-export CLEAN_DIRTY="sh ""$HOME""/automation/utils_clean_dirty.sh"
-export KEY_RETURN="sh ""$HOME""/automation/utils_sendkey_Return.sh"
+export CLEAN="$SHELL"" ""$AUTOMATION""/utils_clean.sh"
+export CLEAN_DIRTY="$SHELL"" ""$AUTOMATION""/utils_clean_dirty.sh"
+export KEY_RETURN="$SHELL"" ""$AUTOMATION""/utils_sendkey_Return.sh"
 export KEY_TAB="xte \"key Tab\""
 export HIDEFILE="/dev/zero"
-export HIDEFILE_CHECK_DELETE="sh ""$HOME""/automation/utils_hidefile_check_delete.sh"
-export LOG="sh ""$HOME""/automation/utils_log.sh $0"
+export HIDEFILE_CHECK_DELETE="$SHELL"" ""$AUTOMATION""/utils_hidefile_check_delete.sh"
+export LOG="$SHELL"" ""$AUTOMATION""/utils_log.sh $0"
 
 SCREENRESOLUTION="$(ps aux | grep -v grep | grep -i Xtightvnc | sed 's/ -geometry /\n/g' | tail -n1 | sed 's/ /\n/g' | head -n1 | sed 's/x/\n/g')"
 if [ -n "$SCREENRESOLUTION" ]; then
@@ -17,8 +19,8 @@ else
 	export SCREENWIDTH=$(xrandr | head -n1 | sed 's/current /\n/' | tail -n1 | sed 's/, /\n/g' | head -n1 | sed 's/ x /\n/' | head -n1)
 fi
 
-export WAITLOADED="nice -n 19 sh ""$HOME""/automation/utils_waitLoaded_loop.sh"
-export WAITLOADED_SILENT="nice -n 19 sh ""$HOME""/automation/utils_waitLoaded_loop_silent.sh"
+export WAITLOADED="nice -n 19 sh ""$AUTOMATION""/utils_waitLoaded_loop.sh"
+export WAITLOADED_SILENT="nice -n 19 sh ""$AUTOMATION""/utils_waitLoaded_loop_silent.sh"
 
 export DOWNLOADS="$HOME""/Downloads"
 
