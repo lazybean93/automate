b9687e111058150d3efecd5b21a9007062a8e67e
Oliver Tiebe <oliver.tiebe@gmail.com>
2025-05-14 16:41:21 +0200

    modified:   Kleinanzeigen/closeAtInsert.sh

--- Kleinanzeigen/closeAtInsert.sh
+++ Kleinanzeigen/closeAtInsert.sh
@@ -6,7 +6,7 @@ sh "$HOME""/automation/firefox_private.sh" "www.google.com"
 
 "$SHELL" -c "$WAITLOADED 2"
 
-if [ -z "$(wmctrl -l | grep 'Deine Anzeige geht bald online')" ]; then
+if [ -z "$(wmctrl -l | grep 'Google')" ]; then
     echo FAIL!
     exit 0
 fi
